6cc83fb08e753df37a65561629fa88044f783262
Talon Meyer <talonmeyer12@gmail.com>
2023-04-06 19:30:47 -0700

    testing heroku stuff

--- config/connection.js
+++ config/connection.js
@@ -4,21 +4,31 @@ require('dotenv').config();
 // create connection to the db. Connect to JWASDB instance if env var provided.
 // (Heroku creates this variable when you provision a JAWSDB instance.)
 
-let sequelize;
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  // use local .env file for connection arguments
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: '127.0.0.1',
       dialect: 'mysql',
-      port: 3306,
-    }
-  );
-}
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+// let sequelize;
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   // use local .env file for connection arguments
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: '127.0.0.1',
+//       dialect: 'mysql',
+//       port: 3306,
+//     }
+//   );
+// }
 
 module.exports = sequelize;
